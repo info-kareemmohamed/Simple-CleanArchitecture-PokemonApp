c4b11de89aee883904951673bed91e492cd5cd42
Kareem Mohamed <63722@fa-hiss.edu.eg>
2024-07-26 18:07:46 +0300

    handle dagger hilt

---
+++ app/src/main/java/com/example/simplecleanarchitecturepokemonapp/di/RetrofitModule.kt
@@ -0,0 +1,31 @@
+package com.example.simplecleanarchitecturepokemonapp.di
+
+import com.example.simplecleanarchitecturepokemonapp.common.Constants.BASE_URL
+import com.example.simplecleanarchitecturepokemonapp.data.remote.PokemonApi
+import com.example.simplecleanarchitecturepokemonapp.data.repository.PokemonRepositoryImpl
+import com.example.simplecleanarchitecturepokemonapp.domain.repository.PokemonRepository
+import dagger.Module
+import dagger.Provides
+import dagger.hilt.InstallIn
+import dagger.hilt.components.SingletonComponent
+import retrofit2.Retrofit
+import javax.inject.Singleton
+
+@Module
+@InstallIn(SingletonComponent::class)
+object RetrofitModule {
+
+    @Provides
+    @Singleton
+    fun providePokemonApi(): PokemonApi {
+        return Retrofit.Builder().baseUrl(BASE_URL).build().create(PokemonApi::class.java)
+    }
+
+    @Provides
+    @Singleton
+    fun providePokemonRepository(api: PokemonApi): PokemonRepository {
+        return PokemonRepositoryImpl(api)
+    }
+
+
+}
--- app/src/main/java/com/example/simplecleanarchitecturepokemonapp/presentation/ui/MainActivity.kt
+++ app/src/main/java/com/example/simplecleanarchitecturepokemonapp/presentation/ui/MainActivity.kt
@@ -1,11 +1,14 @@
-package com.example.simplecleanarchitecturepokemonapp
+package com.example.simplecleanarchitecturepokemonapp.presentation.ui
 
 import android.os.Bundle
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import com.example.simplecleanarchitecturepokemonapp.R
+import dagger.hilt.android.AndroidEntryPoint
 
+@AndroidEntryPoint
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
