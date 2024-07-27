fc8417005728b90522fe69ebccd2a6877e87b2d5
Kareem Mohamed <63722@fa-hiss.edu.eg>
2024-07-27 19:09:05 +0300

    splash screen

--- app/src/main/java/com/example/simplecleanarchitecturepokemonapp/presentation/MainActivity.kt
+++ app/src/main/java/com/example/simplecleanarchitecturepokemonapp/presentation/MainActivity.kt
@@ -1,8 +1,9 @@
-package com.example.simplecleanarchitecturepokemonapp.presentation.ui
+package com.example.simplecleanarchitecturepokemonapp.presentation
 
 import android.os.Bundle
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 import com.example.simplecleanarchitecturepokemonapp.R
@@ -12,6 +13,8 @@ import dagger.hilt.android.AndroidEntryPoint
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        Thread.sleep(1000)
+        installSplashScreen()
         enableEdgeToEdge()
         setContentView(R.layout.activity_main)
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
@@ -19,5 +22,9 @@ class MainActivity : AppCompatActivity() {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
+
+
     }
+
+
 }
