415574b35b0e635e3d9655724d7f519d03f02134
Kareem Mohamed <63722@fa-hiss.edu.eg>
2024-07-27 19:07:46 +0300

    navigation component

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,7 +1,7 @@
 plugins {
     alias(libs.plugins.android.application)
     alias(libs.plugins.jetbrains.kotlin.android)
-
+    id("androidx.navigation.safeargs.kotlin")
     id("kotlin-kapt")
     id("com.google.dagger.hilt.android")
 }
@@ -36,6 +36,9 @@ android {
     kotlinOptions {
         jvmTarget = "1.8"
     }
+    buildFeatures {
+        dataBinding = true
+    }
     kapt {
         correctErrorTypes = true
     }
@@ -48,17 +51,23 @@ dependencies {
     implementation(libs.material)
     implementation(libs.androidx.activity)
     implementation(libs.androidx.constraintlayout)
+    implementation(libs.androidx.navigation.fragment.ktx)
+    implementation(libs.androidx.navigation.ui.ktx)
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
-
+    implementation("org.jetbrains.kotlinx:kotlinx-coroutines-core:1.7.0")
+    implementation("org.jetbrains.kotlinx:kotlinx-coroutines-android:1.7.0")
     ///retrofit
     implementation(libs.retrofit)
     implementation (libs.converter.gson)
-
-
-    ///
+    ///hilt
     implementation(libs.hilt.android)
     kapt(libs.hilt.android.compiler)
+    //splashscreen
+    implementation(libs.androidx.core.splashscreen)
+    /////COIL
+    implementation(libs.coil)
+
 
 }
